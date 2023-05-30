6a74fb9a948894d054ec41b05c8d065f773193c5
sama-s-ai <83381890+sama-s-ai@users.noreply.github.com>
2023-05-30 14:00:47 +0100

    Update session84_productExpansion.sql

--- productAnalysis/session84_productExpansion.sql
+++ productAnalysis/session84_productExpansion.sql
@@ -18,17 +18,15 @@ create temporary table time_period
 
 SELECT
 	website_session_id,
-CASE 
+	CASE 
 	WHEN created_at < '2013-12-12' THEN 'A. Pre_Birthday_Bear' 
-    WHEN created_at >= '2013-12-12' THEN 'B. Post_Birthday_Bear' 
-    ELSE 'Uh oh...check logic'
-END AS time_period
+    	WHEN created_at >= '2013-12-12' THEN 'B. Post_Birthday_Bear' 
+    	ELSE 'Uh oh...check logic'
+	END AS time_period
 
 FROM website_sessions
 
 WHERE created_at BETWEEN '2013-11-12' AND '2014-01-12';
-
-SELECT * from time_period;
 
 -- This is the query that I'll use as a sub-query once happy with the data pulled in.
 
@@ -53,21 +51,20 @@ SELECT
 	SUM(items_purchased) / COUNT(DISTINCT order_id) as products_per_order,
 	SUM(price_usd) / COUNT(DISTINCT website_session_id) as revenue_per_session
 
-FROM
-(	-- sub query
+FROM	( -- sub query	
 	SELECT 
-	time_period.time_period,
-	time_period.website_session_id,
-	orders.order_id,
-	orders.items_purchased,
-	orders.price_usd
+		time_period.time_period,
+		time_period.website_session_id,
+		orders.order_id,
+		orders.items_purchased,
+		orders.price_usd
 
 	FROM time_period
 		LEFT JOIN website_sessions
 			ON time_period.website_session_id = website_sessions.website_session_id
 
-	LEFT JOIN orders
-	ON time_period.website_session_id = orders.website_session_id
+		LEFT JOIN orders
+			ON time_period.website_session_id = orders.website_session_id
 
 ) as my_sub
 
