9bcef08188ac7c3199adbf62cc92d6fe55503f51
sama-s-ai <83381890+sama-s-ai@users.noreply.github.com>
2023-06-08 11:50:01 +0100

    Update final_project_complete.sql

--- finalProject/final_project_complete.sql
+++ finalProject/final_project_complete.sql
@@ -16,3 +16,24 @@ FROM website_sessions
 			WHERE website_sessions.created_at < '2015-01-01' -- decided to remove the 2015 quarter since it's incomplete
 GROUP BY 1,2
 ORDER BY 1,2;
+
+/*
+2. Next, let’s showcase all of our efficiency improvements. I would love to show quarterly figures 
+since we launched, for session-to-order conversion rate, revenue per order, and revenue per session. 
+
+*/
+
+SELECT
+	YEAR(website_sessions.created_at) as yr,
+	QUARTER(website_sessions.created_at) as qtr,
+	COUNT(DISTINCT orders.order_id) / COUNT(DISTINCT website_sessions.website_session_id) AS session_to_order_conv_rate, 
+    SUM(orders.price_usd) / COUNT(DISTINCT order_id) as revenue_per_order,
+    SUM(orders.price_usd) / COUNT(DISTINCT website_sessions.website_session_id) as rev_per_session
+
+FROM website_sessions
+	LEFT JOIN orders
+		USING (website_session_id)
+			WHERE website_sessions.created_at < '2015-01-01' -- decided to remove the 2015 quarter since it's incomplete
+GROUP BY 1,2
+ORDER BY 1,2;
+
