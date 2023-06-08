9aa0d22e8fa76640da8e1e8cb5fda1ee6328d2a9
sama-s-ai <83381890+sama-s-ai@users.noreply.github.com>
2023-06-08 12:01:19 +0100

    Update final_project_complete.sql
    q3 complete

--- finalProject/final_project_complete.sql
+++ finalProject/final_project_complete.sql
@@ -37,3 +37,22 @@ FROM website_sessions
 GROUP BY 1,2
 ORDER BY 1,2;
 
+/*
+3. I’d like to show how we’ve grown specific channels. Could you pull a quarterly view of orders 
+from Gsearch nonbrand, Bsearch nonbrand, brand search overall, organic search, and direct type-in?
+*/
+
+SELECT
+    YEAR(website_sessions.created_at) AS yr,
+    QUARTER(website_sessions.created_at) AS qtr,
+    COUNT(DISTINCT CASE WHEN utm_source = 'gsearch' AND utm_campaign = 'nonbrand' THEN orders.order_id ELSE NULL END) AS gsearch_nonbrand_orders,
+    COUNT(DISTINCT CASE WHEN utm_source = 'bsearch' AND utm_campaign = 'nonbrand' THEN orders.order_id ELSE NULL END) AS bsearch_nonbrand_orders,
+    COUNT(DISTINCT CASE WHEN utm_campaign = 'brand' THEN orders.order_id ELSE NULL END) AS brand_search_orders,
+    COUNT(DISTINCT CASE WHEN utm_source IS NULL AND http_referer IS NOT NULL THEN orders.order_id ELSE NULL END) AS organic_search_orders,
+    COUNT(DISTINCT CASE WHEN utm_source IS NULL AND http_referer IS NULL THEN orders.order_id ELSE NULL END) AS direct_type_in_orders
+FROM website_sessions
+    LEFT JOIN orders
+        USING (website_session_id)
+WHERE website_sessions.created_at < '2015-01-01' -- Decided to remove the 2015 quarter since it's incomplete
+GROUP BY 1, 2
+ORDER BY 1, 2;
